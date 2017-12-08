addd3115b889cedd5e60a27b9f87d2afb61a2fee
Dino Lozina <dino.lozina2017@gmail.com>
2017-12-08 12:37:35 +0100

    rucno_cam_control

--- Vizijski_Omco_WPF/PRucno.xaml.cs
+++ Vizijski_Omco_WPF/PRucno.xaml.cs
@@ -25,65 +25,14 @@ namespace VizijskiSustavWPF
         //private HDevelopExport HDevExp;
 
         
-        float currentPosX = 0.0f, currentPosY = 0.0f, currentPosR = 0.0f;
-
-        public static readonly DependencyProperty distance = DependencyProperty.Register("Distance", typeof(float), typeof(PRucno), new PropertyMetadata());
-        public float Distance
-        {
-            get { return (float)GetValue(distance); }
-            set { SetValue(distance, value); }
-        }
-
-        public static readonly DependencyProperty firstPoint = DependencyProperty.Register("FirstPoint", typeof(float), typeof(PRucno), new PropertyMetadata(0.0f, new PropertyChangedCallback(OnPointChange)));
-        public float FirstPoint
-        {
-            get { return (float)GetValue(firstPoint); }
-            set { SetValue(firstPoint, value); }
-        }
-
-        public static readonly DependencyProperty secondPoint = DependencyProperty.Register("SecondPoint", typeof(float), typeof(PRucno), new PropertyMetadata(0.0f, new PropertyChangedCallback(OnPointChange)));
-        public float SecondPoint
-        {
-            get { return (float)GetValue(secondPoint); }
-            set { SetValue(secondPoint, value); }
-        }
-
-        // New dependency property for automatic measurement
-
-        public static readonly DependencyProperty unosruba = DependencyProperty.Register("Unosruba", typeof(float), typeof(PRucno), new PropertyMetadata());
-        public float Unosruba
-        {
-            get { return (float)GetValue(unosruba); }
-            set { SetValue(unosruba, value); }
-        }
-
-        public static readonly DependencyProperty prvirub = DependencyProperty.Register("Prvirub", typeof(float), typeof(PRucno), new PropertyMetadata());
-        public float Prvirub
-        {
-            get { return (float)GetValue(prvirub); }
-            set { SetValue(prvirub, value); }
-        }
-
-        public static readonly DependencyProperty drugirub = DependencyProperty.Register("Drugirub", typeof(float), typeof(PRucno), new PropertyMetadata());
-        public float Drugirub
-        {
-            get { return (float)GetValue(drugirub); }
-            set { SetValue(drugirub, value); }
-        }
-
-
-
         public PRucno() // Constructor
         {
 
-            
-           
             InitializeComponent();
-
             App.HDevExp = new HDevelopExport(); // New Object
             App.PLC.Update_100_ms += new PLCInterface.UpdateHandler(updatePagePRucno_100ms);
             App.PLC.Update_1_s += new PLCInterface.UpdateHandler(updatePagePRucno_1s);
-      
+
         }
 
         private void RunExport() // Method
@@ -135,14 +84,13 @@ namespace VizijskiSustavWPF
 
         private void Button_Click_5(object sender, RoutedEventArgs e)
         {
+           
             
         }
 
         private void Button_Click_6(object sender, RoutedEventArgs e)
         {
             //App.HDevExp.InitHalcon();
-
-
             //Thread exportThread = new Thread(new ThreadStart(this.RunExport));
             //exportThread.Start();
         }
@@ -154,104 +102,13 @@ namespace VizijskiSustavWPF
         
         }
 
-        private void b_snimiPrvi_Click(object sender, RoutedEventArgs e)
-        {
-                //this.Dispatcher.Invoke(new Action(() =>
-                //{
-                //    lock (HDevelopExport.HalconLock)
-                //    {
-                //        FirstPoint = (((float)App.HDevExp.hv_output.D / 3) * 0.005f) + (float)App.PLC.STATUS.HorizontalnaOs.AktualnaPozicija.Value;
-                        
-                //    }
-                //}));
-               
-            
-        }
-
-        private void b_snimiDrugi_Click(object sender, RoutedEventArgs e)
-        {
-            //lock (HDevelopExport.HalconLock)
-            //{
-            //    this.Dispatcher.Invoke(new Action(() =>
-            //    {
-            //        SecondPoint = (((float)App.HDevExp.hv_output.D / 3) * 0.005f) + (float)App.PLC.STATUS.HorizontalnaOs.AktualnaPozicija.Value;
-            //    }));
-
-            //}
-        }
-
-        private void of_actualPosX_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void bb_rasvjetaDimenzije_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void if_setpointPosX_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void bb_automatikaStart_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void bb_jogPlusX_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void updatePagePRucno_1s(object sender, PLCInterfaceEventArgs e)
         {
-         
 
-        
+
         }
-
-
-
-
-        private static void OnPointChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            //PRucno promjenaKontrola = (PRucno)d;
-            //// Korekcija 10 um
-            //promjenaKontrola.Distance=Math.Abs(promjenaKontrola.FirstPoint-promjenaKontrola.SecondPoint) + 0.010f;
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
+
 }
