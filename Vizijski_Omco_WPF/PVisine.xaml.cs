2c29659a341f579021f0664a17b7691122ff9fbf
Dino Lozina <dino.lozina2017@gmail.com>
2018-01-17 08:24:13 +0100

    cleared_visine_page

---
+++ Vizijski_Omco_WPF/PVisine.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using OxyPlot;
+using OxyPlot.Wpf;
+using System.IO;
+
+namespace VizijskiSustavWPF
+{
+    /// <summary>
+    /// Interaction logic for PSrh.xaml
+    /// </summary>
+    public partial class PVisine : Page
+    {
+        
+        public PVisine()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void updatePage(object sender, PLCInterfaceEventArgs e)
+        {
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+    }
+}
